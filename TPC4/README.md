5f55eea74fdd9fe02eb9473430850d39349e9eed
a104445 <a104445@alunos.uminho.pt>
2025-03-18 21:21:14 +0000

    tpc4

---
+++ TPC4/README.md
@@ -0,0 +1,13 @@
+# Analisador Léxico
+## A104445 - Alexandre Marques Miranda
+### ![](../imagens/fotoRelatorio.webp)
+### 8/3/2025
+
+Para a resolução deste trabalho, foi criado o ficheiro TPC4. Este ficheiro está dividido em 2 partes principais:
+
+- divisão dos tokens
+- definição da expressão regular dos tokens
+
+Em relação à divisão de tokens, foram definidos os tokens para as palavras reservadas como "select", "where" e "LIMIT", as variáveis que começam por '?', as propriedades de DBO e FOAF, as chavetas a abrir e a fechar, o ponto, as strings, os idiomas, os numeros e os atalhos.
+
+Para as expressões regulares dos tokens, foram utilizadios os símbolos "{", "}", "." para as chavetas a abrir, a fechar e para o ponto, respetivamente, a letra 'a' para os atalhos, as palavras "select", "where", "LIMIT" para as palavras reservadas, as variáveis, que começam por '?' e possuem pelo menos uma letra, as propriedades de dbo e foaf começam pela respetiva propriedade, seguidas de ':' e por fim o nome associado, os números que são uma sequência de dígitos, as strings que estão entre """ e os idiomas que vêm após o '@'.  
---
+++ TPC4/TPC4.py
@@ -0,0 +1,92 @@
+import re
+import ply.lex as lex
+
+texto = """
+select ?nome ?desc where {
+?s dbo:MusicalArtist.
+?s foaf:name "Chuck Berry"@en .
+?w dbo:artist ?s.
+?w foaf:name ?nome.
+?w dbo:abstract ?desc
+} LIMIT 1000
+"""
+
+tokens = (
+    'SELECT', 
+    'WHERE', 
+    'LIMIT', 
+    'VAR', 
+    'DBO', 
+    'FOAF', 
+    'ACHA',
+    'FCHA', 
+    'PONTO', 
+    'STRING_LITERAL',
+    'IDIOMA', 
+    'NUM',
+    'A' 
+)
+
+t_ACHA = r'\{'
+t_FCHA = r'\}'
+t_PONTO = r'\.'
+t_A = r'a'
+
+def t_SELECT(t):
+    r'select'
+    return t
+
+def t_WHERE(t):
+    r'where'
+    return t
+
+def t_LIMIT(t):
+    r'LIMIT'
+    return t
+
+
+def t_VAR(t):
+    r'[?][a-zA-Z][a-zA-Z0-9_]*'
+    return t
+
+def t_DBO(t):
+    r'dbo:[a-zA-Z]+'
+    return t
+
+
+def t_FOAF(t):
+    r'foaf:[a-zA-Z]+'
+    return t
+
+def t_NUM(t):
+    r'[0-9]+'
+    return t
+
+def t_STRING_LITERAL(t):
+    r'"(.+)"'
+    return t
+
+def t_IDIOMA(t):
+    r'@(.+)'
+    return t
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Erro léxico: caracter ilegal '{t.value[0]}'")
+    t.skip(1)
+    
+
+lexer = lex.lex()
+
+lexer.input(texto)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
